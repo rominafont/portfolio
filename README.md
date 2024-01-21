b91530919349f89ada9cd5d69ae130d7e33d6283
rominafont <romifont.rf@alumnos.cei.es>
2024-01-21 19:27:11 +0100

    LAST COMMIT 21/01
    Proyecto listo para ser evaluado.

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# portfolio
-Welcome to my design portfolio!  In this repository, you will find a collection of my best work, showcasing my skills in user interface design, user experience design, branding, and illustration.
+
+# Romina Font - Portfolio
+Bienvenidos a mi portfolio hecho en enero de 2024
+
+
+# Sobre mi portfolio
+
+Esta web es una web que pretende estar en constante evolución según avancen mis conocimientos en como frontend y visual designer. Este es el primer proyecto web que hago programado enteramente por mi profesionalemente. Por lo que es la carta de visita perfecta para saber cual es mi grado de conocimientos en cuanto a HTML, CSS y JasaScript. 
+
+He decidido aplicar estilos que ahora son tendencia en el mundo del siño digital como el glassmorphism o los degradados animados, todo ello aplicado desde css. 
+
+Aquí tambien se muestran todas mis facetas como creativa: diseñadora gráfica, uxui, fotógrafa e ilustradora. 
+
+## Lenguajes utilizados
+
+- HTML
+- CSS
+- JavaScript
+
+## Branding 
+Branding sencillo que pretende no destacar entre los proyectos que se muestran. 
+
+
+### Paleta de color:
+Mi paleta pasa todos los controles de accesibilidad y la extraje de https://palett.es/ esta web analiza colores y testea que sean funcionales y visibles para todas las personas.
+- BG-color: #fcfcfc;
+- Titles-color: #112d2a;
+- Black-color: #111111;
+- White-color: #EEEEEE;
+- Highlight-color: #e2866d;
+
+![Alt text](./img/readme/paletaPortfolio.svg)
+https://palett.es/1b423e-5c6660-e2866d-d9b2d7-e3cfed
+
+### Tipografías (Google Fonts):
+-Lato: como fuente principal con 3 variables de peso(400,700,900)
+-Instrument italic para efectos y animaciones específicas
+
+![Alt text](./img/readme/tiposPortfolio.png)
+
+
+## Moodboard
+Antes de empreder el camino diseñando, prototipando y desarrollando tuve una sesión de brainstorming e inspiración donde cree este tablero.
+
+
+![Alt text](./img/readme/moodboardPortfolio.png)
+
+### Webs alucinantes que me encantaron:
+- https://radilson.com/
+- https://www.ivettefelixuy.com/
+- https://www.griflan.com/
+- https://shelyvaisman.com/
+- https://www.iamtamara.design/
+
+
+## Prototipo primitivo en Excalidraw
+
+![Alt text](./img/readme/prototipoPortfolio.png)
+
+https://excalidraw.com/#json=AQlqKmwcqEFCk0K02TV2o,UM7N7jmMPacU13t0AkxxCQ
+
+
+
+## Herramientas de organización y gestión del tiempo
+- Trello
+- Notion
+
+![Alt text](./img/readme/trelloPortfolio.png)
+
+
+# Páginas del proyecto
+
+## Landing principal
+Landing principal con galerías de visualización de proyectos e imagenes. 
+
+
+![Alt text](./img/readme/index.png)
+
+## Proyectos
+En esta página se muestran tarjetas con previews a mis proyectos, al click te lleva a ver en detalle el proyecto.
+
+
+![Alt text](./img/readme/projects.png)
+
+## View
+Página donde se muestra en detalle cada proyecto.
+
+
+![Alt text](./img/readme/projects.png)
+
+## Galeria
+En esta página muestros fotografías e ilustraciones mediante una galeria responsive que al click en cada foto abre un modal con la imagen en full size.
+
+
+![Alt text](./img/readme/gallery.png)
+![Alt text](./img/readme/openPhoto.png)
+
+## About en formato grid 
+Cada celda es una información diferente sobre y en el segundo donde se muestra mi fotografía hay un input para saludar.
+
+
+![Alt text](./img/readme/about.png)
+
+
+# Contacto
+Si te ha gustado este proyecto no dudes en ponerte en contacto conmigo, ¡me haría muchísima ilusión!
+- Mail: romina.create@gmail.com
+- Instagram: romina.create
+- LinkedIn: Romina Font Regalado
--- js/main.js
+++ js/main.js
@@ -5,47 +5,47 @@
 const navTopPC = document.getElementById("navTopPC");
 const navTopMobile = document.getElementById("NavTopMobile");
 
-// Event when dimensions change
+// vento para cuando cambian las dimensiones de la pantalla
 window.addEventListener("resize", () => {
-    // <768px, hidden navTopPC and add NavTopMobile
+    // <768px, elimina navTopPC y añade  NavTopMobile
     if (window.innerWidth < 768) {
         navTopPC.classList.add("hidden");
         navTopMobile.classList.remove("hidden");
     } else {
-        // >=768px, hidden NavTopMobile and add navTopPC
+        // >=768px, elimina NavTopMobile y añade navTopPC
         navTopPC.classList.remove("hidden");
         navTopMobile.classList.add("hidden");
     }
 });
-// Call the event when refresh
+// llama al evento cuando se refresca la pantalla
 window.dispatchEvent(new Event("resize"));
 
 
 
-// NavTop block disappear when scrolling down and appear when scrolling up
+// NavTop block desaparece al hacer scroll hacia abajo y aparece de nuevo al scroll hacia arriba
 const navTop = document.getElementById('navTopPC');
 let prevScrollPos = window.pageYOffset;
 
-    // Verify the dimensions
-    if (window.innerWidth > 768) {
-        let prevScrollPos = window.pageYOffset;
+// verifica las dimensiones
+if (window.innerWidth > 768) {
+    let prevScrollPos = window.pageYOffset;
 
-        // Add and remove the navTopPC depending on the scroll direction
-        window.addEventListener('scroll', () => {
-            const currentScrollPos = window.pageYOffset;
+    // añade y quita el bloque segun el tipo de scroll arriba o abajo
+    window.addEventListener('scroll', () => {
+        const currentScrollPos = window.pageYOffset;
 
-            if (prevScrollPos > currentScrollPos) {
-                navTop.classList.remove('hidden');
-            } else {
-                navTop.classList.add('hidden');
-            }
+        if (prevScrollPos > currentScrollPos) {
+            navTop.classList.remove('hidden');
+        } else {
+            navTop.classList.add('hidden');
+        }
 
-            prevScrollPos = currentScrollPos;
-        });
-    }
+        prevScrollPos = currentScrollPos;
+    });
+}
 
 
-// Open Modal Menu 
+// Se abre el menu mobil modal 
 let isNavTopModalOpen = false;
 
 function navTopModal() {
@@ -60,20 +60,234 @@ function navTopModal() {
 }
 
 
+
+
+//SCRIPT INDEX
 // Carrousel Animado: la imagen cambia cada 5s
 let currentSlide = 1;
-          
-            function goToSlide(slideNumber) {
-              const slider = document.getElementById('slider');
-              const slideWidth = slider.clientWidth;
-              slider.scrollLeft = (slideNumber - 1) * slideWidth;
-              currentSlide = slideNumber;
+
+function goToSlide(slideNumber) {
+    const slider = document.getElementById('slider');
+    const slideWidth = slider.clientWidth;
+    slider.scrollLeft = (slideNumber - 1) * slideWidth;
+    currentSlide = slideNumber;
+}
+
+function autoSlide() {
+    currentSlide = (currentSlide % 3) + 1;
+    goToSlide(currentSlide);
+}
+
+// Cambiar de slide automáticamente cada 5 segundos
+setInterval(autoSlide, 5000);
+
+
+
+
+//SCRIPT PROJECTS: pagina donde se muestran tarjetas a modo de "preview" de los proyectos
+// fetch para traer el json con los proyectos
+document.addEventListener("DOMContentLoaded", function () {
+    const projectsContainer = document.getElementById("projectsContainer");
+
+
+    const jsonUrl = "./database/projects.json"; //ruta hacia el json
+
+    fetch(jsonUrl)
+        .then((response) => response.json())
+        .then((projects) => {
+            projects.forEach((project) => {
+                const projectCard = document.createElement("article");
+                projectCard.classList.add("ProjectsBox-card");
+                //bloque html se ejecuta junto con los datos que extraemos del fetch
+                projectCard.innerHTML = `
+          <a href="./view.html?id=${project.id}">
+            <img alt="${project.name}" class="ProjectsBox-img" src="${project.img}">
+            <div class="ProjectsBox-info">
+                <h2 class="ProjectsBox-title Title">${project.name}</h2>
+                <div class="ProjectsBox-tags Text">
+                    <p class="ProjectsBox-role">${project.role}</p>
+                    <p class="ProjectsBox-type">${project.type}</p>
+                    <p class="ProjectsBox-date">${project.date}</p>
+                </div>
+            </div>
+            <h3 class="MobileDetails Title">${project.name}</h3>
+        </a>
+          `;
+
+                projectsContainer.appendChild(projectCard);
+            });
+        })
+        .catch((error) => console.error("Error fetching project information:", error));
+});
+
+
+//SCRIPT VIEW (pagina vacia donde se carga os datos segun el click en la pestaña anterior PROJECTS)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectDetailsContainer = document.getElementById("projectDetails");
+
+    // se obtiene la ID del proyecto de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const miId = urlParams.get("id");
+
+    console.log("parametros:", urlParams);
+
+    // fetch para obtener los detalles del proyecto según el ID
+    fetch("./database/projects.json")
+        .then((response) => response.json())
+        .then((projects) => {
+            // busca el proyecto con la ID seleccionada
+            const projectChoose = projects.find((project) => project.id == Number(miId));
+
+            if (projectChoose) {
+                // estructura html de la pagina para mostrar los datos del fetch
+                projectDetailsContainer.innerHTML = `
+            <img class="ViewContainer-header" src="${projectChoose.img}" alt="Photo">
+            <h1 class="ViewContainer-title Title">${projectChoose.name}</h1>
+            <div class="ViewContainer-data">
+                <div class="ViewContainer-tags">
+                    <p class="ViewContainer-date Subtitle">${projectChoose.date}</p>
+                    <p class="ViewContainer-role Subtitle">${projectChoose.role}</p>
+                    <p class="ViewContainer-type Subtitle">${projectChoose.type}</p>
+                </div>
+                <p class="ViewContainer-text Text">${projectChoose.descp}</p>
+            </div>
+            <img src="${projectChoose.img}" alt="Photo" class="ViewContainer-img">
+            <div class="ViewContainer-url"><a class="Btn" href="${projectChoose.url}" target="_blank" rel="noopener noreferrer">View Site</a></div>
+          `;
+            } else {
+                projectDetailsContainer.innerHTML = `<p class="Title">Proyecto no encontrado</p>`;
             }
-          
-            function autoSlide() {
-              currentSlide = (currentSlide % 3) + 1;
-              goToSlide(currentSlide);
-            }
-          
-            // Cambiar de slide automáticamente cada 5 segundos
-            setInterval(autoSlide, 5000);
+        })
+        .catch((error) => console.error("Error fetching project information:", error));
+});
+
+
+//SCRIPT GALLERY: pagina galeria donde al hacer click se abre la imagen en fullscreen y las imagenes vienen desde un json
+
+
+/* Al click de ancla del banner 1 el scroll es smooth*/
+// Se seleccionan todos los enlaces que comienzan con "#"
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener('click', function (e) {
+        // previene que que pegue el "salto" al hacer scroll
+        e.preventDefault();
+
+        // se obtiene el valor del id gracias al href
+        const targetId = this.getAttribute('href');
+
+        // se obtiene la posición del id, el destino
+        const targetElement = document.querySelector(targetId);
+
+        // verifica si el destino existe
+        if (targetElement) {
+            // se realiza el scroll de manera suave (smooth)
+            targetElement.scrollIntoView({
+                behavior: 'smooth' // el desplazamiento es suave
+            });
+        }
+    });
+});
+
+
+
+// Traemos las fotografias desde el JSON
+document.addEventListener("DOMContentLoaded", function () {
+    // selecciona el contenedor de galeria al que me dirijo
+    const galleryBox = document.getElementById("galleryPhotographs");
+
+    // URL del archivo JSON con las URLs de las imágenes
+    const jsonUrl = "./database/photographs.json";
+
+    // Realiza la solicitud Fetch para obtener el JSON
+    fetch(jsonUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((imageData) => {
+                // Crea un elemento de imagen
+                const img = document.createElement("img");
+                img.classList.add("Gallery-img");
+                img.loading = "lazy"; // carga lento para mayor rendimiento
+                img.src = imageData.url; // el url a la carpeta donde esta la imagen
+                img.title = imageData.title; // el titulo de la foto
+
+                // Agrega las imagenes al contenedor de la galería
+                galleryBox.appendChild(img);
+            });
+
+            // Código para abrir la imagen en full screen
+            galleryBox.addEventListener("click", function (event) {
+                // Verifica si el elemento clicado es una imagen con la clase 'Gallery-img'
+                if (event.target.classList.contains("Gallery-img")) {
+                    // Alterna la clase 'openPhoto' en la imagen clicada
+                    event.target.classList.toggle("openPhoto");
+                }
+            });
+        })
+        .catch((error) => console.error("Error fetching images:", error));
+});
+
+
+// Traemos las ilustraciones desde el JSON
+document.addEventListener("DOMContentLoaded", function () {
+    // selecciona el contenedor de galeria al que me dirijo
+    const galleryBox = document.getElementById("galleryIllustrations");
+
+    // URL del archivo JSON con las URLs de las imágenes
+    const jsonUrl = "./database/illustrations.json";
+
+    // Realiza la solicitud Fetch para obtener el JSON
+    fetch(jsonUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((imageData) => {
+                // Crea un elemento de imagen
+                const img = document.createElement("img");
+                img.classList.add("Gallery-img");
+                img.loading = "lazy"; // carga lento para mayor rendimiento
+                img.src = imageData.url; // el url a la carpeta donde esta la imagen
+                img.title = imageData.title; // el titulo de la foto
+
+                // Agrega las imagenes al contenedor de la galería
+                galleryBox.appendChild(img);
+            });
+
+            // Código para abrir la imagen en full screen
+            galleryBox.addEventListener("click", function (event) {
+                // Verifica si el elemento clicado es una imagen con la clase 'Gallery-img'
+                if (event.target.classList.contains("Gallery-img")) {
+                    // Alterna la clase 'openPhoto' en la imagen clicada
+                    event.target.classList.toggle("openPhoto");
+                }
+            });
+        })
+        .catch((error) => console.error("Error fetching images:", error));
+});
+
+
+// /* La imagen al click se abre en fullscreen (funciona con imagenes cargadas en el HTML no con fetch)*/
+// window.onload = () => {
+//   for (let i of document.querySelectorAll(".Gallery-img")) {
+//     i.onclick = () => i.classList.toggle("openPhoto");
+//   }
+// };
+
+
+
+
+//SCRIPT ABOUT
+// Funcion que saluda al usuario con su nombre obtenido desde un input
+function sayHi() {
+    //obtenemos el valor del input
+    const name = document.getElementById('name').value;
+
+    // construimos el mensaje que se va a mostrar
+    const sayHi = `Hi! ${name} nice to meet you! 🌸🫰🏼`;
+
+    // se muestra el resultado en su bloque salute con una animacion
+    const saluteDiv = document.getElementById('salute');
+    saluteDiv.innerHTML = sayHi;
+    saluteDiv.classList.add('fadeIn');
+    saluteDiv.style.display = 'block';
+}
